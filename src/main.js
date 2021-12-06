e86ec26ca816668d96d72158ec7d72063ef057bf
Maks <makssenuk2002@gmail.com>
2021-12-06 16:40:04 +0300

    finished

--- src/main.js
+++ src/main.js
@@ -3,11 +3,33 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Vuelidate from 'vuelidate'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: "AIzaSyDORus25JH8eoWc2t6-Zq0fSmLeBlRRq64",
+  authDomain: "vue-todo-75fe2.firebaseapp.com",
+  projectId: "vue-todo-75fe2",
+  storageBucket: "vue-todo-75fe2.appspot.com",
+  messagingSenderId: "26876629563",
+  appId: "1:26876629563:web:22e818d79994a72dba021e"
+})
+
+let app
+firebase.auth().onAuthStateChanged(()=>{
+  if(!app){
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+  state:{
+    name:localStorage.name
+  },
+  getters:{
+    name: s => s.name,
   },
   mutations: {
   },
